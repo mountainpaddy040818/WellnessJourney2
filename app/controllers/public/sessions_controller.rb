06946bc0983c4b7503642d9dfa796ed33d09198a
Akihiro Yamada <153415348+mountainpaddy040818@users.noreply.github.com>
2024-03-07 12:21:02 +0000

    ヘッダーフッター装飾/ログイン時のエラー解消

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -19,7 +19,7 @@ class Public::SessionsController < Devise::SessionsController
   # end
 
   protected
-  
+
   # アクティブであるかを判断するメソッド
   def user_state
     # 【処理内容1】 入力されたemailからアカウントを1件取得
@@ -30,15 +30,14 @@ class Public::SessionsController < Devise::SessionsController
     return unless user.valid_password?(params[:user][:password])
     # 【処理内容4】アクティブでない会員に対する処理
     if user.is_active == true
-    # is_activeの値がtrueだった場合、createアクションを実行させるためにuser_stateメソッドを終了する  
-      return user_state
+    # is_activeの値がtrueだった場合、createアクションを実行させるためにuser_stateメソッドを終了する
     else
-    # is_activeの値がfalseだった場合、サインアップ画面に遷移する処理を実行する  
+    # is_activeの値がfalseだった場合、サインアップ画面に遷移する処理を実行する
       redirect_to sign_up_path
-    end 
+    end
+  end 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  def configure_sign_in_params
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   end
 end
