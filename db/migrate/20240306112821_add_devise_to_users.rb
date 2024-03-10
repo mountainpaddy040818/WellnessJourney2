556d34012bcaccf9af4d3a265d4a3b98ff8471a3
Akihiro Yamada <153415348+mountainpaddy040818@users.noreply.github.com>
2024-03-10 09:34:43 +0000

    修正

--- db/migrate/20240306112821_add_devise_to_users.rb
+++ db/migrate/20240306112821_add_devise_to_users.rb
@@ -5,16 +5,16 @@ class AddDeviseToUsers < ActiveRecord::Migration[6.1]
     change_table :users do |t|
       ## Database authenticatable
       # t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      # t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
+      # t.string   :reset_password_token
+      # t.datetime :reset_password_sent_at
 
       ## Rememberable
-      t.datetime :remember_created_at
+      # t.datetime :remember_created_at
 
-      t.string :name, null: false, default: ""
+      # t.string :name, null: false, default: ""
       # 名前入力
       t.text :introduction, null: false
       # 自己紹介文
