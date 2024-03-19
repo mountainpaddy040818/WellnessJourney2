a1bc0cbf45114411e05938cc85e53786e0168904
Akihiro Yamada <153415348+mountainpaddy040818@users.noreply.github.com>
2024-03-19 07:06:17 +0000

    Action Mailer

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'a@a'
+  config.mailer_sender = ENV['WELCOME_MAILER_ADDRESS']
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
