5a61a58ec33693fe21d6ca6588bf1a34d028e5f7
Akihiro Yamada <153415348+mountainpaddy040818@users.noreply.github.com>
2024-03-21 11:57:32 +0000

    表示時間の日本語化

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module WellnessJourney
   class Application < Rails::Application
+    config.time_zone = 'Tokyo'
+    config.i18n.default_locale = :ja
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
