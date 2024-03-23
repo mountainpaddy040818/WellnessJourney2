a00e69d50435503f15d15dc13d33bba6e5d4abc6
Akihiro Yamada <153415348+mountainpaddy040818@users.noreply.github.com>
2024-03-23 09:56:13 +0000

    管理者機能不具合修正

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,7 +2,7 @@ class Group < ApplicationRecord
   has_many :group_users, dependent: :destroy
   belongs_to :owner, class_name: 'User'
   has_many :users, through: :group_users, source: :user, dependent: :destroy
-  
+
   def includesUser?(user)
     group_users.exists?(user_id: user.id)
   end
@@ -10,7 +10,11 @@ class Group < ApplicationRecord
   validates :name, presence: true
   validates :introduction, presence: true
   has_one_attached :group_image
-  
+
+  def get_group_image
+    (group_image.attached?) ? group_image : 'no_image.jpg'
+  end
+
   def is_owned_by?(user)
     owner.id == user.id
   end
