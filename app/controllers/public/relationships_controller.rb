65af94450269d68826804c081b5356aebd7791ea
Akihiro Yamada <153415348+mountainpaddy040818@users.noreply.github.com>
2024-03-25 05:48:30 +0000

    フォロー機能/非同期通信化

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -2,17 +2,19 @@ class Public::RelationshipsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    user = User.find(params[:user_id])
-    current_user.follow(user)
-    redirect_back(fallback_location: root_path)
+    @user = User.find(params[:user_id])
+    current_user.follow(@user)
+    render 'btn'
+    # redirect_back(fallback_location: root_path)
   end
 
   def destroy
-    user = User.find(params[:user_id])
-    current_user.unfollow(user)
-    redirect_back(fallback_location: root_path)
+    @user = User.find(params[:user_id])
+    current_user.unfollow(@user)
+    render 'btn'
+    # redirect_back(fallback_location: root_path)
   end
-  
+
   # def create
   #   @user = User.find(params[:user_id])
   #   following = Relationship.create(follower_id: params[:user_id], following_id: current_user.id)
