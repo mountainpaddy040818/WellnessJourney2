7ab0d009cb2b1e217de17cada7bd5fc6282ba578
Akihiro Yamada <153415348+mountainpaddy040818@users.noreply.github.com>
2024-03-27 12:47:18 +0000

    翻訳エラー解消

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -34,20 +34,6 @@ class Public::SessionsController < Devise::SessionsController
   end
 
   protected
-    # # アクティブであるかを判断するメソッド
-    # def user_state
-    # #   # 【処理内容1】 入力されたemailからアカウントを1件取得
-    #   user = User.find_by(email: params[:user][:email])
-    # #   # 【処理内容2】 アカウントを取得できなかった場合、このメソッドを終了する
-    #   return if user.nil?
-    # #   # 【処理内容3】 取得したアカウントのパスワードと入力されたパスワードが一致していない場合、このメソッドを終了する
-    #   return unless user.valid_password?(params[:user][:password])
-    # #   # 【処理内容4】アクティブでない会員に対する処理
-    #   if user.is_active == false
-    #     redirect_to new_user_registration_path,
-    #     notice: "You have already canceled your membership. Please register again to use our service."
-    #   end
-    # end
 
     # # If you have extra params to permit, append them to the sanitizer.
     def configure_sign_in_params
--- app/models/record_tag.rb
+++ app/models/record_tag.rb
@@ -1,6 +1,5 @@
 class RecordTag < ApplicationRecord
   has_many :health_record_tags, dependent: :destroy
   has_many :health_records, through: :health_record_tags
-
   validates :tag_name, length: {maximum:50}
 end
