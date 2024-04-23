da5a96ec762060c06e0e2df826e4064e671ded30
Akihiro Yamada <153415348+mountainpaddy040818@users.noreply.github.com>
2024-04-23 14:51:35 +0000

    いいね機能バグ発見・修正

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,10 +1,6 @@
 class Public::FavoritesController < ApplicationController
   def create
     @health_record = HealthRecord.find(params[:health_record_id])
-    # すでにいいねされていれば処理を抜ける
-    if @health_record.find_by(user_id: current_user.id).nil?
-      return
-    end
     favorite = current_user.favorites.new(health_record_id: @health_record.id)
     favorite.save
   end
