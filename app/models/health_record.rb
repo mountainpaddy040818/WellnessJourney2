a8a58d3e3c1557c952ec39f7d59e6a3e887e525e
Akihiro Yamada <153415348+mountainpaddy040818@users.noreply.github.com>
2024-04-24 11:46:34 +0000

    いいね順に並び替え

--- app/models/health_record.rb
+++ app/models/health_record.rb
@@ -2,6 +2,8 @@ class HealthRecord < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   has_many :favorites, dependent: :destroy
+  has_many :week_favorites, -> { where(created_at: 1.week.ago.beginning_of_day..Time.current.end_of_day) }
+
   has_many :health_record_comments, dependent: :destroy
 
   has_many :health_record_tags, dependent: :destroy
