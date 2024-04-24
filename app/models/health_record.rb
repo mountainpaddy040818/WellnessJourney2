ff9f8b6a4e17a459b5c42fa913241816c28b6c73
Akihiro Yamada <153415348+mountainpaddy040818@users.noreply.github.com>
2024-04-24 12:43:34 +0000

    いいね順に並び替える時の条件変更

--- app/models/health_record.rb
+++ app/models/health_record.rb
@@ -2,7 +2,6 @@ class HealthRecord < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   has_many :favorites, dependent: :destroy
-  has_many :week_favorites, -> { where(created_at: 1.week.ago.beginning_of_day..Time.current.end_of_day) }
 
   has_many :health_record_comments, dependent: :destroy
 
