9699f133f6c8e468f13c622fc0ef6b5842bac3bf
Akihiro Yamada <153415348+mountainpaddy040818@users.noreply.github.com>
2024-05-05 14:04:04 +0000

    komenntoauto

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,9 +2,10 @@ class Group < ApplicationRecord
   has_many :group_users, dependent: :destroy
   # "owner"をユーザーモデルに関連づけるため
   belongs_to :owner, class_name: "User"
-  # 
+  # 中間テーブル"group_users"を介してアソシエーション
   has_many :users, through: :group_users, source: :user, dependent: :destroy
-
+  
+  # 同じユーザーが２回同じグループに参加するのを防ぐため
   def includesUser?(user)
     group_users.exists?(user_id: user.id)
   end
--- app/models/record_tag.rb
+++ app/models/record_tag.rb
@@ -1,5 +1,7 @@
 class RecordTag < ApplicationRecord
   has_many :health_record_tags, dependent: :destroy
+  
+  # 中間テーブル"health_record_tags"を介してアソシエーション
   has_many :health_records, through: :health_record_tags
   validates :tag_name, length: {maximum:50}
 end
